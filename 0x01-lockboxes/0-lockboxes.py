822fad79ca95c8317118ea980800d65ba5d9e3fa
Brianoyaro <brianoyaro2000@yahoo.com>
2024-05-01 17:54:21 +0300

    second commit

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,20 +3,16 @@
 
 
 def canUnlockAll(boxes):
-    """Determines if all boxes can be unlocked using DFS starting from box 0."""
-    stack = [0]  # Start with the first box (index 0)
-    visited = [False] * len(boxes)
-    visited[0] = True  # Mark the first box as visited
-    
+    """Determines if all boxes can be unlocked from box 0."""
+    stack = [0]
+    # visited = [False] * len(boxes)
+    visited = [False for i in range(len(boxes))]
+    visited[0] = True
     while stack:
-        current_box = stack.pop()  # Pop from the stack to perform DFS
-        
+        current_box = stack.pop()
         # Iterate through keys in the current box
         for key in boxes[current_box]:
             if 0 <= key < len(boxes) and not visited[key]:
-                # If the key leads to a valid, unvisited box, mark it as visited and add it to the stack
                 visited[key] = True
                 stack.append(key)
-    
-    # Check if all boxes have been visited
     return all(visited)
