b9c5d00d8efa1150b141006fbf4367433799fc31
Brianoyaro <brianoyaro2000@yahoo.com>
2024-05-01 17:31:50 +0300

    second commit

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,5 +1,9 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+"""module solving locked boxes"""
+
+
 def canUnlockAll(boxes):
+    """determines if all boxes can be unlocked"""
     stack = [0]
     unlocked = [0]
     tracker = [0]
@@ -13,7 +17,6 @@ def canUnlockAll(boxes):
                 unlocked.append(val)
         tracker.append(stack[0])
         stack.remove(stack[0])
-       # print('unlocked: {}'.format(unlocked))
     for val in unlocked:
         if val in range(len(boxes)):
             check[val] = True
