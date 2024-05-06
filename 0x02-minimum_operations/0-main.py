a52a2af79abc9c76541509c357ccbaf2bd3f1b72
Brianoyaro <brianoyaro2000@yahoo.com>
2024-05-06 21:22:53 +0300

    initial commit

---
+++ 0x02-minimum_operations/0-main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Main file for testing
+"""
+
+minOperations = __import__('0-minoperations').minOperations
+
+n = 4
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 12
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+print("Min # of operations to reach {} char: {}".format(17, minOperations(17)))
+n=120
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+n=9
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+n=1001
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+n=3
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+def getPrime(start):
+    while True:
+        if (start % 2 == 0) or (start % 3 == 0) or (start % 5 == 0) or (start % 7 == 0):
+            if start not in [2, 3, 5, 7]:
+                start += 1
+            else:
+                return start
+                break
+        else:
+            return start
+            break
+
+def minOperations(n):
+    val = 0
+    prime = 2
+    temp = n
+    while n > 1:
+        while (n % prime != 0):
+            prime = getPrime(prime + 1)
+        if temp < prime:
+            return 0
+        n /= prime
+        if (n == 1) and (prime == temp):
+            return 0
+        val += prime
+    return val
