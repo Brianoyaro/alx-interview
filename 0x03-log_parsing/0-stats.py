0f7d394ef3eac223c83f17b39198df7ed405bccd
Brianoyaro <brianoyaro2000@yahoo.com>
2024-05-14 14:58:11 +0300

    task 0

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,43 +2,25 @@
 """log parsing module"""
 import sys
 import re
+import signal
 
 
 reg_exp = r"[\d\.]+[]\d\-\.:\s[]+\"GET /projects/260 HTTP/1.1\"[\d\s]+"
-allowed_status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
-lines = sys.stdin.read()
-lines = lines.split('\n')
+allowed_code = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 file_size = 0
-i = 10
-terminate_point = 0
-try:
-    while lines[-1] != lines[terminate_point]:
-        group = []
-        mark = 0
-        while mark < i:
-            line = lines[mark]
-            gateman = re.match(reg_exp, line)
-            '''if line doesn't match regex expression, skip it'''
-            if gateman.group(0) != line:
-                continue
-            file_size += int(line.split()[-1])
-            status_code = line.split()[-2]
-            if int(status_code) in allowed_status_codes:
-                group.append(status_code)
-            mark += 1
-        # write logic for grouping and mapping each status_code to its count
-        unique_set = list(set(group))
-        unique_set.sort()
-        mapper = {}
-        for val in unique_set:
-            sum_ = group.count(val)
-            mapper[val] = sum_
-        # first print File size: file_size -> line 21
+total_lines = 0
+for line in sys.stdin:
+    gateman = re.match(reg_exp, line)
+    '''if line doesn't match regex expression, skip it'''
+    if gateman.group(0) == line:
+        total_lines += 1
+        file_size += int(line.split()[-1])
+        status_code = line.split()[-2]
+        if int(status_code) in allowed_code:
+            allowed_code[int(status_code)] += 1
+
+    if (total_lines % 10 == 0):
         print("File size: {}".format(file_size))
-        # then print all mapped values
-        for key, value in mapper.items():
-            print("{}: {}".format(key, value))
-        i += 10
-        terminate_point += 10
-except KeyboardInterrupt as e:
-    pass
+        for key, value in allowed_code.items():
+            if value > 0:
+                print("{}: {}".format(key, value))
