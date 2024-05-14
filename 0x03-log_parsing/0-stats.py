5d6900bf095490c1eb1495ba780fbcfe1b54e815
Brianoyaro <brianoyaro2000@yahoo.com>
2024-05-14 17:02:31 +0300

    task 0

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -7,6 +7,9 @@ import signal
 
 def signal_handler(sig, frame):
     """sigint handler"""
+    for key, value in allowed_code.items():
+        if value > 0:
+            print("{}: {}".format(key, value))
     sys.exit(0)
 
 
