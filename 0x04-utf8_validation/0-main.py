d0482aad76887a1a719ac71d687108f5f96df10b
Brianoyaro <brianoyaro2000@yahoo.com>
2024-05-20 21:02:20 +0300

    initial commit

---
+++ 0x04-utf8_validation/0-main.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Main file for testing
+"""
+
+validUTF8 = __import__('0-validate_utf8').validUTF8
+
+data = [65]
+print(validUTF8(data))
+
+data = [80, 121, 116, 104, 111, 110, 32, 105, 115, 32, 99, 111, 111, 108, 33]
+print(validUTF8(data))
+
+data = [229, 65, 127, 256]
+print(validUTF8(data))
+
+# custom
+data = [36]
+print(validUTF8(data))
+data = [194, 163]
+print(validUTF8(data))
+data = [208, 152]
+print(validUTF8(data))
+data = [224, 164, 185]
+print(validUTF8(data))
+data = [226, 130, 172]
+print(validUTF8(data))
+data = [237, 149, 156]
+print(validUTF8(data))
+data = [240, 144, 141, 136]
+print(validUTF8(data))
+data = [244, 137, 154, 179]
+print(validUTF8(data))
+'''invalid test case
+'''
+data = [244, 36, 137, 154, 179]
+print(validUTF8(data))
---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+'''utf-8validation
+'''
+import re
+from typing import List
+
+
+def binary_function(n: int) -> str:
+    '''returns a binary representation of a number
+    '''
+    bin_n = bin(n)[2:]
+    padding = 8 - len(bin_n)
+    for _ in range(padding):
+        bin_n = '0' + bin_n
+    return bin_n
+
+
+def check_type(s: str) -> int:
+    '''finds type of code point associated with a byte
+    '''
+    type_1_regex = r"^0[\d]{7}"
+    type_2_regex = r"^110[\d]{5}"
+    type_3_regex = r"^1110[\d]{4}"
+    type_4_regex = r"^11110[\d]{3}"
+    val = re.match(type_1_regex, s)
+    if val is not None:
+        return 1
+    val = re.match(type_2_regex, s)
+    if val is not None:
+        return 2
+    val = re.match(type_3_regex, s)
+    if val is not None:
+        return 3
+    val = re.match(type_4_regex, s)
+    if val is not None:
+        return 4
+    return -1
+
+
+def is_next_valid(data: List) -> bool:
+    '''validates next data set byte
+    '''
+    val = data.pop(0)
+    bin_item = binary_function(val)
+    check = re.match(r'^10[\d]{6}', bin_item)
+    '''if check is None:
+        return False
+    return True
+    '''
+    return check
+
+
+def validUTF8(data: List[int]) -> bool:
+    '''determines if a given data set represents a valid UTF-8 encoding
+    '''
+    while data:
+        item = data.pop(0)
+        '''byte can range from 0 - 255
+        '''
+        if item > 255:
+            return False
+        bin_item = binary_function(item)
+        type_item = check_type(bin_item)
+        if type_item == 1:
+            continue
+        if type_item == 2:
+            '''next item should comply
+            '''
+            check = is_next_valid(data)
+            if check is None:
+                return False
+            continue
+        if type_item == 3:
+            '''next 2 items should comply
+            '''
+            for _ in range(2):
+                check = is_next_valid(data)
+                if check is None:
+                    return False
+
+        if type_item == 4:
+            '''next 3 items should comply
+            '''
+            for _ in range(3):
+                check = is_next_valid(data)
+                if check is None:
+                    return False
+        if type_item == -1:
+            return False
+    return True
